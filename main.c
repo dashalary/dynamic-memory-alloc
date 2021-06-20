5c4867cae6928c4abd9e7890bf7861cd5a4a9833
Dasha Lary <dasha4988@gmail.com>
2021-06-20 17:40:25 -0400

    ran build

--- main.c
+++ main.c
@@ -3,5 +3,25 @@
 
 int main()
 {
+    int size;
+    char *text = NULL;
+
+    printf("Enter a limit of the text: \n");
+    scanf("%d", &size);
+
+    text = (char *)malloc(size * sizeof(char));
+
+    if (text != NULL)
+    {
+        printf("Enter some text: \n");
+        scanf(" ");
+        gets(text);
+
+        printf("Inputted text is: %s\n", text);
+    }
+
+    free(text);
+    text = NULL;
+
     return 0;
 }
